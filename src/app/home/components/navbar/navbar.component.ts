30528c1965792501ba8031d35bec5acf577b7934
iarmolla <ivan.armolla@gmail.com>
2023-08-08 16:37:59 -0300

    add profileImage

--- src/app/home/components/navbar/navbar.component.ts
+++ src/app/home/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from '../../interfaces/User';
+import { ImageService } from '../../services/image.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +10,15 @@ import { AuthService } from 'src/app/auth/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isLogged: any
+  isLogged: string
   showOptions: boolean = false
   title: string = 'FINDHOUS'
-  constructor(private authService: AuthService) {
-   this.isLogged = this.authService.subjectIsLogged 
+  userData!: User
+  profileImage$!: string
+  constructor(private authService: AuthService, private imageService: ImageService) {
+    this.imageService.imageSubject.subscribe((data) => this.profileImage$ = data)
+    this.isLogged = this.authService.subjectIsLogged
+    console.log(this.profileImage$);
   }
   options() {
     this.showOptions = !this.showOptions
