dfceedbd08cd5334f83155ac8a2e22f7b0b53793
iarmolla <ivan.armolla@gmail.com>
2023-08-08 16:36:36 -0300

    add User interface

---
+++ src/app/home/interfaces/User.ts
@@ -0,0 +1,13 @@
+export interface User {
+    address: string,
+    birthDate: string,
+    city: string,
+    country: string,
+    description: string
+    email: string
+    image: string
+    lastname: string
+    name: string,
+    postalCode: string,
+    username: string
+}
