bc8eaf257b8ddcb761131bfa8eda48f4d478f2bc
iarmolla <ivan.armolla@gmail.com>
2023-08-18 02:15:59 -0300

    refactor

--- src/app/home/components/property-detail/property-detail.component.ts
+++ src/app/home/components/property-detail/property-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HomeService } from '../../services/home.service';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-property-detail',
@@ -8,11 +9,11 @@ import { HomeService } from '../../services/home.service';
 })
 export class PropertyDetailComponent implements OnInit {
   ownership!: any
-  userImage: string = '../../../../assets/images/user.png'
+  userImage: string = ''
   images!: string[]
-  imagesAux = ['https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/CYB7GHJBS5ALXI33H7A5PWU4WY.jpg&w=1200','https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2014/12/428442-bloodborne-ps4-6-claves.png', 'https://sm.ign.com/t/ign_es/screenshot/default/top10darksoulsbosses1280-1539970285459_m5v4.1280.jpg', 'https://i.ytimg.com/vi/S2mEBYDDKaI/maxresdefault.jpg', 'https://assets1.ignimgs.com/thumbs/userUploaded/2016/4/7/every-boss-thumb-1460050305472.jpg', 'https://whatifgaming.com/wp-content/uploads/2023/01/1674921439367-1024x576.jpg', 'https://assetsio.reedpopcdn.com/dark_souls_3_yhorm_the_giant.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp']
-  constructor(private router: Router, private homeService: HomeService, private activatedRoute: ActivatedRoute) { }
+  constructor(private router: Router, private homeService: HomeService, private activatedRoute: ActivatedRoute, private imageService: ImageService) { }
   ngOnInit() {
+    this.userImage = this.imageService.userImage
     this.activatedRoute.queryParams.subscribe((data): any => {
       this.ownership = data
       console.log(data);
